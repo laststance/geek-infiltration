81f905f1f62311343dbc363c211b9531c02c3036
ryota-murakami <dojce1048@gmail.com>
2022-05-17 05:24:01 +0900

    make request

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,7 +1,18 @@
 import { memo } from 'react'
 
 const SignIn = () => {
-  return <div>SignIn</div>
+  return (
+    <div>
+      <h1>SignIn</h1>
+      <a
+        href={`https://github.com/login/oauth/authorize?scope=user&client_id=${
+          import.meta.env.VITE_CLIENT_ID
+        }&redirect_uri=${import.meta.env.VITE_REDIRECT_URI}`}
+      >
+        GitHub Login
+      </a>
+    </div>
+  )
 }
 
 export default memo(SignIn)
--- src/atom.ts
+++ src/atom.ts
@@ -1,4 +1,3 @@
 import { atom } from 'jotai'
 
-export const loginAtom = atom(false)
 export const userAtom = atom(null)
