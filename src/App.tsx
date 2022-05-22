bfb02880b856003666208340d236105d5113f583
ryota-murakami <dojce1048@gmail.com>
2022-05-22 20:43:53 +0900

    little

---
+++ src/App.tsx
@@ -0,0 +1,76 @@
+import {
+  Card,
+  Container,
+  Row,
+  Col,
+  Text,
+  Link,
+  Spacer,
+} from '@nextui-org/react'
+import { useAtomValue } from 'jotai'
+import React from 'react'
+
+import { accessTokenAtom } from './atom'
+import { useGetIssueCommentsQuery } from './generated/graphql'
+import Loading from './Loading'
+
+const endpoint = 'https://api.github.com/graphql'
+
+function App() {
+  const accessToken = useAtomValue(accessTokenAtom)
+
+  const { status, data, error, isFetching } = useGetIssueCommentsQuery(
+    {
+      endpoint: endpoint,
+      fetchParams: { headers: { authorization: `Bearer ${accessToken}` } },
+    },
+    { query: 'markerikson' },
+
+    {
+      // @ts-ignore
+      select: (data) => data!.search!.edges![0]!.node!.issueComments!.edges,
+    }
+  )
+
+  if (!status || status === 'loading' || isFetching)
+    <Container>
+      {' '}
+      <Loading />
+    </Container>
+
+  if (error) <h1>Error</h1>
+
+  return (
+    status === 'success' && (
+      <Container>
+        <Col offset={1}>
+          {data.reverse().map(({ node }, i) => (
+            <Row key={i} gap={0.5}>
+              <Card bordered shadow={false} css={{ mw: '400px' }}>
+                <Text color="primary">{node.repository.nameWithOwner}</Text>
+                <Link underline href={node.issue.url} target="_blank">
+                  {node.issue.title}{' '}
+                  <Text small>{node.issue.author.login}</Text>
+                  <Text small>{new Date(node.createdAt).toLocaleString()}</Text>
+                  <Spacer />
+                </Link>
+                <Card>
+                  <Text
+                    size={15}
+                    dangerouslySetInnerHTML={{ __html: node.bodyHTML }}
+                  />
+                </Card>
+              </Card>
+            </Row>
+          ))}
+        </Col>
+        <Col offset={1}>
+          <Card>fwiejfeawiofjewoi</Card>
+          <Card>jfoiwejfiowefjweoi</Card>
+        </Col>
+      </Container>
+    )
+  )
+}
+
+export default App
