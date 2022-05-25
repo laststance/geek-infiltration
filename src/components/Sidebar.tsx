a00f75d54152fd0e2492b1ad7dda4497d1f8efb4
ryota-murakami <dojce1048@gmail.com>
2022-05-26 04:55:56 +0900

    up

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,24 @@
-import { Card, Avatar, Divider } from '@nextui-org/react'
-import React, { memo } from 'react'
-
-import PlusButton from './PlusButton'
+import {
+  Card,
+  Avatar,
+  Divider,
+  Button,
+  Modal,
+  Text,
+  Input,
+  Row,
+  Checkbox,
+} from '@nextui-org/react'
+import React, { memo, useState, useCallback } from 'react'
 
 const Sidebar = () => {
+  const [visible, setVisible] = useState(false)
+  const handler = useCallback(() => setVisible(true), [])
+
+  const closeHandler = useCallback(() => {
+    setVisible(false)
+  }, [])
+
   return (
     <Card as="section" css={{ borderRadius: 0, h: '100%' }}>
       <Card.Body
@@ -16,7 +31,57 @@ const Sidebar = () => {
           p: '20px 10px',
         }}
       >
-        <PlusButton />
+        <Button
+          auto
+          size="md"
+          color="gradient"
+          shadow
+          css={{ border: 0, fontSize: '30px', padding: '0 15px' }}
+          onClick={handler}
+        >
+          +
+        </Button>
+        <Modal
+          closeButton
+          aria-labelledby="modal-title"
+          open={visible}
+          onClose={closeHandler}
+        >
+          <Modal.Header>
+            <Text id="modal-title" size={18}>
+              Search{' '}
+              <Text b size={18}>
+                GitHub Username
+              </Text>
+            </Text>
+          </Modal.Header>
+          <Modal.Body>
+            <Input
+              clearable
+              bordered
+              fullWidth
+              color="primary"
+              size="lg"
+              placeholder="ryota-murakami"
+            />
+            <Row justify="space-between">
+              <Checkbox>
+                <Text size={14}>Issue/PR/Discussion Comments</Text>
+              </Checkbox>
+              <Checkbox>
+                <Text size={14}>Opened Issue/PR/Discussion</Text>
+              </Checkbox>
+            </Row>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button auto onClick={closeHandler}>
+              Search
+            </Button>
+            <Button auto flat color="error" onClick={closeHandler}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Card.Body>
       <Divider />
       <Card.Footer blur as="footer" css={{ p: '20px 10px' }}>
