843cdf81d506e8b15f10b46aa01444677d05f18f
ryota-murakami <dojce1048@gmail.com>
2022-12-20 19:02:50 +0900

    named

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -14,62 +14,64 @@ interface Props {
   bodyHTML: string
 }
 
-const CommentCard: React.FC<Props> = ({
-  author,
-  repositoryName,
-  ticketLink,
-  ticketTitle,
-  ticketAuthorName,
-  commentLink,
-  publishedAt,
-  bodyHTML,
-}) => {
-  return (
-    <Card
-      as="section"
-      bordered
-      shadow
-      css={{
-        borderBottomColor: 'rgba(77, 77, 77, 0.7)',
-        borderBottomWidth: '1px',
-        borderTopColor: 'rgba(77, 77, 77, 0.7)',
-        borderTopWidth: '1px',
-      }}
-    >
-      <Text color="primary" h5>
-        <Link target="_blank" href={ticketLink}>
-          {repositoryName}
-        </Link>
-      </Text>
-      <Row as="section">
-        <Link underline href={commentLink} target="_blank">
-          <Text h4>{ticketTitle}</Text>
-        </Link>
-      </Row>
-      <Row>
-        <Text small>{ticketAuthorName}</Text>
-        <Spacer x={0.4} />
-        <Text small>{new Date(publishedAt).toLocaleString()}</Text>
-      </Row>
-      <Spacer />
-      <Row>
-        <User src={author.avatarUrl} name={author.login} css={{ p: 0 }}>
-          <User.Link href={author.url} target="_blank">
-            @{author.login}
-          </User.Link>
-        </User>
-      </Row>
-      <Row as="section">
-        <div
-          dangerouslySetInnerHTML={{ __html: bodyHTML }}
-          style={{
-            maxWidth: '100%',
-            overflowWrap: 'break-word',
-          }}
-        />
-      </Row>
-    </Card>
-  )
-}
-
-export default memo(CommentCard)
+const CommentCard: React.FC<Props> = memo(
+  ({
+    author,
+    repositoryName,
+    ticketLink,
+    ticketTitle,
+    ticketAuthorName,
+    commentLink,
+    publishedAt,
+    bodyHTML,
+  }) => {
+    return (
+      <Card
+        as="section"
+        bordered
+        shadow
+        css={{
+          borderBottomColor: 'rgba(77, 77, 77, 0.7)',
+          borderBottomWidth: '1px',
+          borderTopColor: 'rgba(77, 77, 77, 0.7)',
+          borderTopWidth: '1px',
+        }}
+      >
+        <Text color="primary" h5>
+          <Link target="_blank" href={ticketLink}>
+            {repositoryName}
+          </Link>
+        </Text>
+        <Row as="section">
+          <Link underline href={commentLink} target="_blank">
+            <Text h4>{ticketTitle}</Text>
+          </Link>
+        </Row>
+        <Row>
+          <Text small>{ticketAuthorName}</Text>
+          <Spacer x={0.4} />
+          <Text small>{new Date(publishedAt).toLocaleString()}</Text>
+        </Row>
+        <Spacer />
+        <Row>
+          <User src={author.avatarUrl} name={author.login} css={{ p: 0 }}>
+            <User.Link href={author.url} target="_blank">
+              @{author.login}
+            </User.Link>
+          </User>
+        </Row>
+        <Row as="section">
+          <div
+            dangerouslySetInnerHTML={{ __html: bodyHTML }}
+            style={{
+              maxWidth: '100%',
+              overflowWrap: 'break-word',
+            }}
+          />
+        </Row>
+      </Card>
+    )
+  }
+)
+CommentCard.displayName = 'CommentCard'
+export default CommentCard
