b25f94e55a70344019fb3fd74cba2d17a645f073
ryota-murakami <dojce1048@gmail.com>
2023-06-06 21:16:03 +0900

    mui theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { NextUIProvider, createTheme } from '@nextui-org/react'
+import { ThemeProvider } from '@mui/material/styles'
 import { Provider as JotaiProvider } from 'jotai'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
@@ -6,6 +6,7 @@ import { QueryClientProvider, QueryClient } from 'react-query'
 
 import './index.css'
 import AuthController from './controllers/AuthController'
+import { theme } from './theme'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,10 +19,6 @@ const queryClient = new QueryClient({
   },
 })
 
-const darkTheme = createTheme({
-  type: 'dark',
-})
-
 // @ts-expect-error Property 'displayName' does not exist on type '({ children, initialValues, scope, unstable_createStore, unstable_enableVersionedWrite, }: PropsWithChildren<{ initialValues?: Iterable<readonly [Atom<unknown>, unknown]> | undefined; scope?: Scope | undefined; unstable_createStore?: ((initialValues?: Iterable<readonly [AnyAtom, unknown]> | undefined) => { ...; }) |...'.Property 'displayName' does not exist on type '({ children, initialValues, scope, unstable_createStore, unstable_enableVersionedWrite, }: PropsWithChildren<{ initialValues?: Iterable<readonly [Atom<unknown>, unknown]> | undefined; scope?: Scope | undefined; unstable_createStore?: ((initialValues?: Iterable<readonly [AnyAtom, unknown]> | undefined) => { ...; }) |...'.
 JotaiProvider.displayName = 'JotaiProvider'
 // @ts-expect-error Property 'displayName' does not exist on type '({ client, children, context, contextSharing, }: QueryClientProviderProps) => Element'.
@@ -30,11 +27,11 @@ QueryClientProvider.displayName = 'QueryClientProvider'
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 
 root.render(
-  <NextUIProvider theme={darkTheme}>
+  <ThemeProvider theme={theme}>
     <JotaiProvider>
       <QueryClientProvider client={queryClient}>
         <AuthController />
       </QueryClientProvider>
     </JotaiProvider>
-  </NextUIProvider>
+  </ThemeProvider>
 )
