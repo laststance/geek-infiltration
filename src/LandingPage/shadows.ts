e788d63ff742208c0386144200325134a7e48c1b
ryota-murakami <dojce1048@gmail.com>
2023-06-21 09:30:40 +0900

    clean

--- src/LandingPage/shadows.ts
+++ src/LandingPage/shadows.ts
@@ -2,10 +2,7 @@
 import { alpha } from '@mui/material/styles'
 import type { Shadows } from '@mui/material/styles/shadows'
 
-//
 import palette from './palette'
-
-// ----------------------------------------------------------------------
 
 interface CustomShadowOptions {
   z1: string
