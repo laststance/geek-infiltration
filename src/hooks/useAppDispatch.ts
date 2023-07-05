456b6193fe1359ce26be60e52b3962831650b75e
ryota-murakami <dojce1048@gmail.com>
2023-07-06 04:32:20 +0900

    fix type

--- src/hooks/useAppDispatch.ts
+++ src/hooks/useAppDispatch.ts
@@ -2,4 +2,4 @@ import { useDispatch } from 'react-redux'
 
 import type { AppDispatch } from '../store'
 
-export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppDispatch = () => useDispatch<AppDispatch>()
