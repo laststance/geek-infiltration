18ed811e9c9144f456b329ba23e18db9023952e2
ryota-murakami <dojce1048@gmail.com>
2023-07-07 21:58:40 +0900

    change icon color and fix type error

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,12 +1,12 @@
 import { createTheme } from '@mui/material/styles'
+import type { ThemeOptions } from '@mui/material/styles'
 
 import { customShadows } from '../LandingPage/shadows'
 
 export const theme = createTheme({
   components: {},
   customShadows: customShadows.dark,
-  // @ts-expect-error @TODO
   palette: {
     mode: 'dark',
   },
-})
+} as ThemeOptions)
