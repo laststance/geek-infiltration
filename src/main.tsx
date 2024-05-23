dfa76551a6020ceb997c414618574bf7bcd62277
Ryota Murakami <dojce1048@gmail.com>
2024-05-23 20:16:24 +0900

    refactorsentry: simplify Sentry initialization by removing browser tracing integration
    - Removed browser tracing integration from Sentry initialization.
    - Kept replay integration for error tracking.
    - This change aims to streamline the Sentry setup and focus on replay

--- src/main.tsx
+++ src/main.tsx
@@ -23,16 +23,7 @@ const persistor = persistStore(store)
 if (import.meta.env.PROD === true) {
   Sentry.init({
     dsn: import.meta.env.VITE_SENTRY_DNS,
-    integrations: [
-      Sentry.browserTracingIntegration({
-        // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-        tracePropagationTargets: [
-          'localhost',
-          /^https:\/\/yourserver\.io\/api/,
-        ],
-      }),
-      Sentry.replayIntegration(),
-    ],
+    integrations: [Sentry.replayIntegration()],
 
     // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0,
