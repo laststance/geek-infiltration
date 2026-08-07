3bc0fde815b6107007fe841de3ef8751c0bfdbe7
Ryota Murakami <dojce1048@gmail.com>
2026-08-07 16:00:57 +0900

    test: enhance swap tests with additional scenarios
    - Updated the swap test suite to include a new test case for swapping elements at the ends of the array.
    - Renamed the test suite to 'swap:happy' for better clarity on the test context.
    - Removed unnecessary skipped tests to improve test coverage.

--- src/utils/swap.test.ts
+++ src/utils/swap.test.ts
@@ -1,12 +1,16 @@
 import { swap } from './swap'
 
-describe('swap', () => {
+describe('swap:happy', () => {
   test('swap item1 and item2', () => {
     const arr = [{ a: 1 }, { b: 2 }, { c: 3 }, { d: 4 }]
     const res = swap(arr, arr[0], arr[2])
     expect(res).toEqual([{ c: 3 }, { b: 2 }, { a: 1 }, { d: 4 }])
   })
-  test.skip('', () => {})
+  test('Ends of an array can be swapped', () => {
+    const arr = [{ a: 1 }, { b: 2 }, { c: 3 }, { d: 4 }]
+    const res = swap(arr, arr[0], arr.at(-1))
+    expect(res).toEqual([{ d: 4 }, { b: 2 }, { c: 3 }, { a: 1 }])
+  })
   test.skip('', () => {})
   test.skip('', () => {})
   test.skip('', () => {})
