5098e5a98b380e572535c66d5a125fe4d0564db4
Ryota Murakami <dojce1048@gmail.com>
2026-08-18 12:59:55 +0900

    fix: replace SWC plugin with React plugin to ensure compatibility and stability

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import { codeInspectorPlugin } from 'code-inspector-plugin'
 import type { IncomingMessage, ServerResponse } from 'node:http'
 import { defineConfig, loadEnv } from 'vite'
